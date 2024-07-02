ada0d7f1f08fff01e855d3e355ece128ada107ed
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-07-02 21:33:16 +0900

    Title: 두 사람, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240702/두 사람/two-person.py
@@ -0,0 +1,9 @@
+age, gender = input().split()
+age2, gender2 = input().split()
+
+if int(age)>=19 and gender=="M":
+    print(1)
+elif int(age2)>=19 and gender2=="M":
+    print(1)
+else:
+    print(0)
