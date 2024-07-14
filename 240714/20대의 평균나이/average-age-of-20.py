224ee899c5511baf39d87d9db831661d599e5d5e
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-07-14 16:36:58 +0900

    Title: 20대의 평균나이, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/20대의 평균나이/average-age-of-20.py
+++ 240714/20대의 평균나이/average-age-of-20.py
@@ -1,9 +1,9 @@
 ages = []
 while True:
     ages.append(int(input()))
-    if ages[-1] >= 30:
+    if ages[-1] >= 30 or ages[-1] < 20:
         break
 if len(ages) == 2:
-    print(ages[0])
+    print(f"{ages[0]:.2f}")
 else:
     print(f"{sum(ages[:-1])/len(ages[:-1]):.2f}")
