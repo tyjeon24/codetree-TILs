6764790962024540dc20f374c06d5baf4bd70f6a
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-07-14 17:47:07 +0900

    Title: 별표 출력하기 13, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/별표 출력하기 13/print-start-13.py
+++ 240714/별표 출력하기 13/print-start-13.py
@@ -1,20 +1,32 @@
 n = int(input())
 
-next_odd_stars = n
-next_even_stars = 1
+# 홀수번째 : n n-1 n-2 n-3 n-3 n-2 n-1 n
+# if i > n//2 기점으로 방향 바꾸면 됨
+# 짝수번째 : 1
+
+toggle = "even"
+even = n
+odd = 1
+for i in range(n):
+    if toggle == "even":
+        print("* "*even)
+        even -= 1
+        toggle = "odd"
+    else:
+        print("* "*odd)
+        odd += 1
+        toggle = "even"
+
+toggle = "even" if toggle=="odd" else "odd"
+odd -= 1
+even += 1
 
 for i in range(n):
-    if i%2==0: # 1, 3, 5, ...
-        print("* "*next_odd_stars)
-        next_odd_stars -= 1
+    if toggle == "even":
+        print("* "*even)
+        even += 1
+        toggle = "odd"
     else:
-        print("* "*next_even_stars)
-        next_even_stars += 1
-
-for j in range(n):
-    if j%2==0: # 1, 3, 5, ...
-        print("* "*next_odd_stars)
-        next_odd_stars -= 1
-    else:
-        print("* "*next_even_stars)
-        next_even_stars += 1
+        print("* "*odd)
+        odd += 1
+        toggle = "even"
