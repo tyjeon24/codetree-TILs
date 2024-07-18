2161ba4c1e070089ae558a0ef0f24f304e84b65a
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-07-18 22:55:29 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240718/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,16 @@
+a = input()
+order = input()
+start_index = 0
+
+for command in order:
+    if command == "L":
+        start_index += 1
+    else:
+        start_index -= 1
+
+if start_index < 0:
+    start_index += len(a)
+for i in range(start_index, len(a)):
+    print(a[i], end="")
+for i in range(0, start_index):
+    print(a[i], end="")
