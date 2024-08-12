f175c4dd68cb192939b249f12b3d780bda8267ef
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-12 21:50:39 +0900

    Title: 힙 정렬 구현, Time: 430ms, Memory: 32MB, Status: Passed - CodeTree

--- 240812/힙 정렬 구현/implement-heap-sort.py
+++ 240812/힙 정렬 구현/implement-heap-sort.py
@@ -1,31 +1,28 @@
-def heapify(arr, n, i):
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-
-    if l < n and arr[l] > arr[largest]:
-        largest = l
-    if r < n and arr[r] > arr[largest]:
-        largest = r
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # Swap
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # Swap
-        heapify(arr, i, 0)
-
-    return arr
-
 n = int(input())
 arr = list(map(int, input().split()))
+arr = [0] + arr
+
+def heapify(n, i):
+    largest = i
+    l = i*2
+    r = i*2+1
+
+    for node in [l, r]:
+        if node <= n and arr[node] > arr[largest]:
+            largest = node
+    
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(n, largest)
+
+def heap_sort(arr):
+    for i in range(n//2, 0, -1):
+        heapify(n, i)
+    for i in range(n, 1, -1):
+        arr[1], arr[i] = arr[i], arr[1]
+        heapify(i-1, 1)
+
 
 heap_sort(arr)
-
-for num in arr:
+for num in arr[1:]:
     print(num, end=" ")
