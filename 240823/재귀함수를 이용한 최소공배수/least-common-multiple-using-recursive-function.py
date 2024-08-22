f99c1990d3337cbc05eae21502506d786ad97e51
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-23 00:07:26 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240823/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,6 +2,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 def calculate(arr, lcm=None, index=1):
+    if len(arr) == 1:
+        print(arr[0])
+        return
     if index==1:
         lcm = arr[0]
 
@@ -9,7 +12,7 @@ def calculate(arr, lcm=None, index=1):
         if lcm % num == 0 and arr[index] % num == 0:
             lcm = int(lcm * arr[index] / num)
             break
-            
+
     if index == len(arr)-1:
         print(lcm)
     else:
