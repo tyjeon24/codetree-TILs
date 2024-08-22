1882514a443a35ea21bb45d61ff9197803801159
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-22 23:36:08 +0900

    Title: 그 계절, 그 날, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,44 @@
+Y, M, D = map(int, input().split())
+
+def is_leap_year(year):
+    result = False
+    if year%4==0:
+        result = True
+        if year%100==0:
+            result = False
+            if year%400==0:
+                result = True
+    return result
+
+def get_season(month):
+    if 3<=month<=5:
+        return "Spring"
+    elif 6<=month<=8:
+        return "Summer"
+    elif 9<=month<=11:
+        return "Fall"
+    else:
+        return "Winter"
+
+last_day = {
+    1:31,
+    2:30,
+    3:31,
+    4:30,
+    5:31,
+    6:30,
+    7:31,
+    8:31,
+    9:30,
+    10:31,
+    11:30,
+    12:31,
+}
+
+if is_leap_year(Y) and M==2 and D>29:
+    print(-1)
+else:
+    if D <= last_day[M]:
+        print(get_season(M))
+    else:
+        print(-1)
