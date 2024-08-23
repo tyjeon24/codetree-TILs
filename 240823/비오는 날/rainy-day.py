7b9ad24f7c081c0a17b857f4431ade544bd35ae6
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-23 21:54:06 +0900

    Title: 비오는 날, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/비오는 날/rainy-day.py
+++ 240823/비오는 날/rainy-day.py
@@ -1,7 +1,12 @@
 n=int(input())
 
+rain_dates = []
+
 for _ in range(n):
     date, day, weather = input().split()
     if weather == "Rain":
-        print(f"{date} {day} {weather}")
-        break
+        y,m,d = map(int, date.split("-"))
+        rain_dates.append((y,m,d,day,weather))
+
+rain_dates = sorted(rain_dates, key=lambda item: (item[0], item[1], item[2]))
+print(f"{rain_dates[0][0]}-{int(rain_dates[0][1]):02d}-{int(rain_dates[0][2]):02d} {rain_dates[0][3]} {rain_dates[0][4]}")
