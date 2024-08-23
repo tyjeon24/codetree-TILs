e59c21d16b7504fcb44af2425070964b3f7cf995
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-24 01:43:40 +0900

    Title: 신기한 타일 뒤집기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240824/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240824/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,22 +1,28 @@
 from collections import deque
 
-queue = deque([0])
+queue = deque()
 idx = 0
 for _ in range(int(input())):
     number, direction = input().split()
     for i in range(int(number)):
         if direction == "L":
-            if idx == 0:
+            if idx >= len(queue):
+                idx = len(queue) - 1
+            if len(queue) == 0 or idx == -1:
                 queue.appendleft("L")
             else:
                 queue[idx] = "L"
                 idx -= 1
         else:
-            if idx == len(queue)-1:
+            if idx < 0:
+                idx = 0
+            if len(queue) == 0 or idx == len(queue):
                 queue.append("R")
+                idx = len(queue)
             else:
                 queue[idx] = "R"
                 idx+=1
+        print(queue)
             
 queue = list(queue)
 print(queue.count("L"), queue.count("R"))
