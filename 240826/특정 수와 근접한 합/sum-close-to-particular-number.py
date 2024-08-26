9c6cefd4ac41b1eac95654162a210ab641bb68ae
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-26 21:45:32 +0900

    Title: 특정 수와 근접한 합, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240826/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240826/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -4,7 +4,7 @@ total = sum(numbers)
 
 result = total
 
-for i in range(N):
+for i in range(N-1):
     for j in range(i+1, N):
         sub_total = total - numbers[i] - numbers[j]
         result = min(result, abs(sub_total - S))
