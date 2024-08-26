16e384856fed9dc980ede5521c383a34550261b4
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-26 22:48:11 +0900

    Title: 밭의 높이를 고르게하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240826/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240826/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -6,9 +6,8 @@ n, h, t = map(int, input().split())
 arr = list(map(int, input().split()))
 
 result = INT_MAX
-for i in range(n-t):
+for i in range(n-t+1):
     sub_arr = arr[i:i+t]
-
     price = 0
     for j in sub_arr:
         price += abs(j-h)
