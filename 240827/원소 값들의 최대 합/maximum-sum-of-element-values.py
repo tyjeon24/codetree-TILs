81749cd7a6b660d7a9344afb565fe406da78e86c
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 22:46:04 +0900

    Title: 원소 값들의 최대 합, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240827/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 240827/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,6 +1,13 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-arr = sorted(arr, reverse=True)
 
-print(sum(arr[:m]))
+result = 0
+for index in range(n):
+    temp = 0
+    next_index = index
+    for _ in range(m):
+        temp += arr[next_index]
+        next_index = arr[next_index]-1
+    result = max(result, temp)
+print(result)
