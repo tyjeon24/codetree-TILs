924b035f49c0ee76ddfb4c0638cb84d4e66f4441
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 23:24:22 +0900

    Title: 전부 겹치는 선분 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -11,19 +11,20 @@ result = "No"
 low = lines[0][1]
 for i in range(n-2):
     left, right = lines[i], lines[i+1]
-    if low != -1 and low < right[0]: # 안 겹치면 break
+    if left[1] < right[0] or low < right[0]: # 안 겹치면 break
         break
     # 두 선 중 더 작은 high 값이 모든 선의 교차점
-    low = min(left[1], right[1], low)
+    low = min(left[1], right[1])
 else:
     result = "Yes"
+
 low = lines[1][1]
 for i in range(1, n-1):
     left, right = lines[i], lines[i+1]
-    if low != -1 and low < right[0]: # 안 겹치면 break
+    if left[1] < right[0] or low < right[0]: # 안 겹치면 break
         break
     # 두 선 중 더 작은 high 값이 모든 선의 교차점
-    low = min(left[1], right[1], low)
+    low = min(left[1], right[1])
 else:
     result = "Yes"
 print(result)
