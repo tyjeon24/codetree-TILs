07c2b300e499564373feb55b20076ea34c9aa357
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 23:33:27 +0900

    Title: 전부 겹치는 선분 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,30 +1,21 @@
+def get_union(l, r):
+    if l[1] < r[0]:
+        return None
+    return (max(l[0], r[0]), min(l[1], r[1]))
+
 n = int(input())
 lines = []
 for _ in range(n):
     low, high = map(int, input().split())
     lines.append((low, high))
-
 lines.sort()
-result = "No"
 
-# 선을 비교
-low = lines[0][1]
-for i in range(n-2):
-    left, right = lines[i], lines[i+1]
-    if left[1] < right[0] or low < right[0]: # 안 겹치면 break
-        break
-    # 두 선 중 더 작은 high 값이 모든 선의 교차점
-    low = min(left[1], right[1])
-else:
-    result = "Yes"
 
-low = lines[1][1]
-for i in range(1, n-1):
-    left, right = lines[i], lines[i+1]
-    if left[1] < right[0] or low < right[0]: # 안 겹치면 break
-        break
-    # 두 선 중 더 작은 high 값이 모든 선의 교차점
-    low = min(left[1], right[1])
+union = lines[1]
+for i in range(2, n-1):
+    union = get_union(union, lines[i+1])
+
+if get_union(lines[0], union) or get_union(union, lines[-1]):
+    print("Yes")
 else:
-    result = "Yes"
-print(result)
+    print("No")
