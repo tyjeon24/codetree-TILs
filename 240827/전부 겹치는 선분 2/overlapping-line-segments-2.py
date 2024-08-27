48472fc42c3f438e74c209f100bd9fab2dd117bf
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 23:15:48 +0900

    Title: 전부 겹치는 선분 2, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,26 +1,33 @@
+n = int(input())
 lines = []
-for _ in range(int(input())):
+for _ in range(n):
     low, high = map(int, input().split())
     lines.append((low, high))
 
 lines.sort()
 result = "No"
-prev_low, prev_high = lines[0]
-for line in lines[1:-1]:
-    current_low, current_high = line[0], line[1]
-    if current_low > prev_high:
+
+# 선을 비교
+low = lines[0][1]
+for i in range(n-2):
+    left, right = lines[i], lines[i+1]
+    
+    if low < right[0]: # 안 겹치면 break
         break
-    prev_low, prev_high = line[0], line[1]
+    # 두 선 중 더 작은 high 값이 모든 선의 교차점
+    low = min(left[1], right[1])
+
 else:
     result = "Yes"
-
-
-prev_low, prev_high = lines[1]
-for line in lines[2:]:
-    current_low, current_high = line[0], line[1]
-    if current_low > prev_high:
+low = lines[1][1]
+for i in range(1, n-1):
+    left, right = lines[i], lines[i+1]
+    
+    if low < right[0]: # 안 겹치면 break
         break
-    prev_low, prev_high = line[0], line[1]
+    # 두 선 중 더 작은 high 값이 모든 선의 교차점
+    low = min(left[1], right[1])
+
 else:
     result = "Yes"
 print(result)
