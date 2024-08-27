6edd02d5dcd0c311457cd3e5b91cf9fdb3ea28a7
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 23:36:21 +0900

    Title: 전부 겹치는 선분 2, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,6 +1,4 @@
-def get_union(left, right):
-    l, r = (left[0], left[1]), (right[0], right[1])
-
+def get_union(l, r):
     if l[1] < r[0]:
         return None
     return (max(l[0], r[0]), min(l[1], r[1]))
@@ -12,12 +10,14 @@ for _ in range(n):
     lines.append((low, high))
 lines.sort()
 
-
 union = lines[1]
 for i in range(2, n-1):
     union = get_union(union, lines[i+1])
-
-if get_union(lines[0], union) or get_union(union, lines[-1]):
-    print("Yes")
+    if union is None:
+        print("No")
+        break
 else:
-    print("No")
+    if get_union(lines[0], union) or get_union(union, lines[-1]):
+        print("Yes")
+    else:
+        print("No")
