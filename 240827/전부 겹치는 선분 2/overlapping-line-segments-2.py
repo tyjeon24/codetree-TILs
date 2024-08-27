e0e8374b5476698a6f54dadb4205ea1fed65359f
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 23:06:27 +0900

    Title: 전부 겹치는 선분 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240827/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -7,20 +7,20 @@ lines.sort()
 result = "No"
 prev_low, prev_high = lines[0]
 for line in lines[1:-1]:
-    current_low, current_high = line
+    current_low, current_high = line[0], line[1]
     if current_low > prev_high:
         break
-    prev_low, prev_high = line
+    prev_low, prev_high = line[0], line[1]
 else:
     result = "Yes"
 
 
 prev_low, prev_high = lines[1]
 for line in lines[2:]:
-    current_low, current_high = line
+    current_low, current_high = line[0], line[1]
     if current_low > prev_high:
         break
-    prev_low, prev_high = line
+    prev_low, prev_high = line[0], line[1]
 else:
     result = "Yes"
 print(result)
