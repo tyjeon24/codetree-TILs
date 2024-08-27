42a2769c6920ee9343477622e13b1c81d30292c3
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-08-27 21:09:48 +0900

    Title: 스승의 은혜 3, Time: 1137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240827/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240827/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -8,21 +8,21 @@ for _ in range(n):
     price, shipping = map(int, input().split())
     prices.append((price + shipping, price, shipping))
 
-prices.sort()
-
 result = 0
 for coupon_target in range(n):
-    new_prices = prices
-    new_prices[coupon_target] = (new_prices[coupon_target][0]-new_prices[coupon_target][1]//2, 0, 0)
+    new_prices = [p for p in prices]
+    new_prices[coupon_target] = (
+        new_prices[coupon_target][0]-new_prices[coupon_target][1]//2,
+        new_prices[coupon_target][1]//2,
+        new_prices[coupon_target][2]
+        )
     new_prices.sort()
     budget = b
     presents = 0
     for i in range(n):
         if budget < new_prices[i][0]:
-            result = max(result, presents)
             break
-        else:
-            budget -= new_prices[i][0]
-            presents += 1
-    result = max(result, presents)
+        budget -= new_prices[i][0]
+        presents += 1
+        result = max(result, presents)
 print(result)
